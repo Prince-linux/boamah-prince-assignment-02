a3280402780403a16e731249649727b680aca1df
Prince Oppong Boamah <regioths@gmail.com>
2022-07-13 18:18:49 +0000

    Check Challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i=0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,7 +32,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumbers = 0;
+  arrayOfNumbers.forEach(function (num) {
+    if (num % 2 === 0) {
+      evenNumbers++;
+    }
+  });
+  return evenNumbers;
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
@@ -47,7 +57,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let fahrenheitArray = [];
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    let convertToFahrenheit = arrayOfNumbers[i] * (9/5) + 32;
+    let finalValue = Math.trunc(convertToFahrenheit);
+    fahrenheitArray.push(finalValue);
+  }
+  return fahrenheitArray;
 }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
